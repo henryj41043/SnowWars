e431e89fc70ccece7ad2d726d7c8ca409f8b31cc
Joshua Henry <henry41043@gmail.com>
2014-06-30 21:15:57 -0400

    Started testing snowball throwing

---
+++ Assets/SnowBallThrowing.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnowBallThrowing : MonoBehaviour {
+	private Vector3 mouseStartPos;
+	private Vector3 mouseEndPos;
+	private bool fire = false;
+	public GameObject snowBall;
+	public float speedMult = 2.0f;
+	
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			mouseStartPos = Input.mousePosition;
+		}
+		if(Input.GetMouseButtonUp(0)){
+			mouseEndPos = Input.mousePosition;
+			fire = true;
+		}
+		if(fire){
+			Fire((mouseStartPos - mouseEndPos).normalized, (mouseEndPos - mouseStartPos).magnitude);
+			fire = false;
+		}
+	}
+	
+	void Fire(Vector3 direction, float speed){
+		GameObject projectile = (GameObject)GameObject.Instantiate(snowBall, this.transform.position, Quaternion.identity);
+		projectile.rigidbody2D.AddForce(direction * speed * speedMult);
+	}
+}
