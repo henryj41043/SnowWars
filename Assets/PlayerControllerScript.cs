fd15173a661ccfb60d3244767cc7d927d78d07f4
Joshua Henry <henry41043@gmail.com>
2014-07-06 21:44:25 -0400

    added movement controller with animations

---
+++ Assets/PlayerControllerScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerScript : MonoBehaviour {
+	public float maxSpeed = 10f;
+	Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		anim.SetFloat("Speed", Mathf.Abs(moveHorizontal));
+		rigidbody2D.velocity = new Vector2(moveHorizontal * maxSpeed, rigidbody2D.velocity.y);
+	}
+}
